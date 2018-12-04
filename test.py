b48248969202857f1b405f02f046bff157a75865
jcraver1021 <elciego@gmail.com>
2018-12-03 21:07:48 -0500

    updated tests for server

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ print(x)
 for i in range(d):
 	print(i, c.update(i, eps))
 
-# Test 2: Multiclient-client output
+# Test 2: Multi-client output
 print("Multi-client output")
 d = 16
 n = 8
@@ -29,18 +29,23 @@ for i in range(d):
 	for j in range(n):
 		print(i, c[j].update(i, eps))
 
-# Test 3: 1-1 client server
+# Test 3: Multi-client server
 print("Single-client server")
 d = 16
+n = 1024
 x = np.random.randint(2, size=d + 1)
-c = longitudinal.Client(x)
+c = []
 s = longitudinal.Server(d)
 eps = 0.1
+for i in range(n):
+	x = np.random.randint(2, size=d + 1)
+	print(x)
+	c.append(longitudinal.Client(x))
 print(x)
 for i in range(d):
-	print(i)
-	report = c.update(i, eps)
-	print(report)
-	s.collect(i, [report])
+	reports = []
+	for j in range(n):
+		reports.append(c[j].update(i, eps))
+	s.collect(i, reports)
 print(s.T)
-s.aggregate(c.k, eps)
+print(s.aggregate(np.max([c[i].k for i in range(n)]), eps))
